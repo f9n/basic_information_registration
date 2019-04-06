b6d094a15b640cd8305f11768122103d1927b7f6
Fatih Sarhan <f9n@protonmail.com>
2019-04-06 20:23:21 +0300

    Add login activity

--- app/src/main/java/io/f9n/basicinformationregistration/MainActivity.java
+++ app/src/main/java/io/f9n/basicinformationregistration/MainActivity.java
@@ -1,13 +1,46 @@
 package io.f9n.basicinformationregistration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usernameEditText, passwdEditText;
+
+    public void bind() {
+        this.usernameEditText = (EditText) findViewById(R.id.username);
+        this.passwdEditText = (EditText) findViewById(R.id.passwd);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.bind();
     }
+
+    public void handleLogIn(View v) {
+        String username = usernameEditText.getText().toString();
+        String passwd = passwdEditText.getText().toString();
+        String defaultAdminName = getString(R.string.defaultAdminName);
+        String defaultAdminPasswd = getString(R.string.defaultAdminPasswd);
+
+        Log.i("login", String.format("Username: %s, Password: %s", username, passwd));
+
+        if (username.equals(defaultAdminName) && passwd.equals(defaultAdminPasswd)) {
+            Log.i("login", "successful");
+            // Intent intent = new Intent(this. HomeActivity.class);
+            // startActivity(intent);
+        } else {
+            Log.i("login", "unsuccessful");
+            Toast.makeText(this, R.string.errorMessageOfWrongLogin, Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
